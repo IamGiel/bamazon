fab598c79bd0310aed02348a4cf90a8aeeb4a2d1
Server <tilesmaster@tiless-mbp.attlocal.net>
2017-12-20 09:38:46 -0500

    edit

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -6,6 +6,7 @@ var fs = require('fs');
 var mysql = require('mysql');
 
 
+
 var connection = mysql.createConnection({
 	host:"localhost",
 	port:"3306",
@@ -21,13 +22,14 @@ var connection = mysql.createConnection({
 function Customer(id, qty) {
 	this.id = [];
 	this.qty = [];
-	
 	var _this = this;
 	
 	//START INTERACTION WITH USER
 	_this.prompt = function (){
 		var resAmt = [];
 		var itemBought = [];
+		var listItems = [];
+		var totalPrice = [];
 
 		// Created a series of questions
 		var question = [{
@@ -70,53 +72,59 @@ function Customer(id, qty) {
 
 
 			    } else
-				if(productID <= 10){
+				  if(productID <= 10){
 				    // Log all results of the SELECT statement
 				    for (var i = 0; i < res.length; i++) {
 		                resAmt.push(res[i].stock_quantity);
 		                itemBought.push(res[i].product_name);
+		                listItems.push(res[i].product_name);
+		                totalPrice.push(res[i].price);
 		                // console.log("THIS IS RESAMT: "+resAmt);
 		                  
 		                if(prodTotal > resAmt){
+		                	// 
 		                   console.log("YOU REQUESTED " + prodTotal + " ITEMS");
 		                   console.log("WE HAVE " + resAmt + " ITEMS LEFT");
-		                   console.log("Oops!, we will get more of that item soon, for now we have " + resAmt + " items in stock.");
-		                   _this.prompt();
-		                }
-		                //UPDATE_ITEM INSIDE READ_ITEM FUNCTION
-		                connection.query("UPDATE products SET ? WHERE ?", 
-		                    [{
-		                      stock_quantity: resAmt - prodTotal
-		                    }, 
-		                    {
-		                      item_id: productID
-		                    }], function(err, res) {
-		                     // console.log("res");
-		                     console.log("Updating database stock quantity...\n");
-		                     if (err) throw err;
-		                     
-		                     // console.log(res);
-		                     console.log(prodTotal + " " + itemBought + " items in your shopping cart!\n");
-		                     
-		                        //REPEAT PROCESS OR EXIT
-		                        inquirer.prompt(morePurchase).then(function(user){
-		                  	  	   if (err) throw err;
-		                  	  	   console.log(user.id);
-		                  	  	   if(user.id == "Maybe later..."){
-		                  	  	 	 console.log("T'was Great to serve you, see you soon!");
-		                  	  	 	 // connection.end();
-		                  	  	 	 return;
-		                  	  	 	
-		                  	  	   } 
-		                  	  	     else if(user.id == "See more cool stuff!..."){
-		                  	  	     	re_readItem();
-		                  	  	     	 _this.prompt();
-		                  	  	  	    
-		                  	  	  }
-		                  	  	});
-		                  	});
-	    			   }
-				    } 
+		                   console.log("Oops!, we will get more of that item soon");
+		                    _this.prompt();
+		                }else
+						if(prodTotal <= resAmt){
+		                    //UPDATE_ITEM INSIDE READ_ITEM FUNCTION
+		                    connection.query("UPDATE products SET ? WHERE ?", 
+		                        [{
+		                          stock_quantity: resAmt - prodTotal
+		                        }, 
+		                        {
+		                          item_id: productID
+		                        }], function(err, res) {
+		                         // console.log("res");
+		                         console.log("Updating database stock quantity...\n");
+		                         if (err) throw err;
+		                         
+		                         // console.log(res);
+		                         console.log(prodTotal + " " + itemBought + " items in your shopping cart!\n");
+		                         
+		                         
+		                            //REPEAT PROCESS OR EXIT
+		                            inquirer.prompt(morePurchase).then(function(user){
+		                      	  	   if (err) throw err;
+		                      	  	   console.log(user.id);
+		                      	  	   if(user.id == "Maybe later..."){
+		                      	  
+		                      	  	 	 console.log(listItems.join(' '), " items in your shopping cart!\n");
+		                      	  	 	 console.log("\n\n\nT'was Great to help with your shopping needs!");
+		                      	  	 	
+		                      	  	   } 
+		                      	  	     else if(user.id == "See more cool stuff!..."){
+		                      	  	     	re_readItem();
+		                      	  	     	 // _this.prompt();
+		                      	  	  	    
+		                      	  	  }
+		                      	  	});
+		                      	});
+		                  }
+	    			  }
+				   } 
 			   });
 			}//end function readItem
 		});
@@ -135,6 +143,8 @@ function re_readItem(){
   	      console.log("PRICE:        " +res[i].price);
           console.log("IN STOCK:     " +res[i].stock_quantity+" left!" +"\n\n\n");
   	   }
+  	   var shopper = new Customer();
+  	   shopper.prompt(); 
 	})
 }
 module.exports = Customer;
