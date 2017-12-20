1a4f7bdb856ec5ba1732440d2d4b94e3b101b406
Server <tilesmaster@tiless-mbp.attlocal.net>
2017-12-19 23:59:56 -0500

    fixed

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,8 +4,7 @@ var connection = require('./bamazon')
 var prompt = require('prompt');
 var fs = require('fs');
 var mysql = require('mysql');
-var resAmt = [];
-var itemBought = [];
+
 
 var connection = mysql.createConnection({
 	host:"localhost",
@@ -22,11 +21,14 @@ var connection = mysql.createConnection({
 function Customer(id, qty) {
 	this.id = [];
 	this.qty = [];
+	
 	var _this = this;
 	
 	//START INTERACTION WITH USER
 	_this.prompt = function (){
-		
+		var resAmt = [];
+		var itemBought = [];
+
 		// Created a series of questions
 		var question = [{
 			type: "input",
@@ -53,78 +55,86 @@ function Customer(id, qty) {
 			productID = user.id;
 			prodTotal = user.quantity;
 			readItem();
-			// updateItem();
-			//connection.query("SELECT * FROM products WHERE item_id="+ productID, function(err, res) {
-			//console.log("THIS IS RESPONSE ", res);
-
-			//});
-
+			
 			//READ ITEM
 			function readItem() {
-			       console.log("Selecting product...\n");
-			       connection.query("SELECT * FROM products WHERE item_id="+ productID, function(err, res) {
-			       console.log("THIS IS RESPONSE ", res);
+			    console.log("Selecting product...\n");
+			    connection.query("SELECT * FROM products WHERE item_id="+ productID, function(err, res) {
+			    // console.log("THIS IS RESPONSE ", res);
 
-			       //checks for VALID user input
-			       if (productID > 10 || isNaN(productID) == true){
-			    	console.log(" == == == CHECK YOUR ITEM ID == == == ");
-			    	
-			    	_this.prompt(); 
+			    //checks for VALID user input
+			    if (productID > 10 || isNaN(productID) == true){
+			   	console.log(" == == == CHECK YOUR ITEM ID == == == ");
+			   	
+			   	_this.prompt(); 
 
 
-			       } else
-				   if(productID <= 10){
-				     // Log all results of the SELECT statement
-				       for (var i = 0; i < res.length; i++) {
-		                  resAmt.push(res[i].stock_quantity);
-		                  itemBought.push(res[i].product_name);
-		                  // console.log("THIS IS RESAMT: "+resAmt);
+			    } else
+				if(productID <= 10){
+				    // Log all results of the SELECT statement
+				    for (var i = 0; i < res.length; i++) {
+		                resAmt.push(res[i].stock_quantity);
+		                itemBought.push(res[i].product_name);
+		                // console.log("THIS IS RESAMT: "+resAmt);
 		                  
-		                  	if(prodTotal > resAmt){
-		                  		console.log("YOU REQUESTED " + prodTotal + " ITEMS");
-		                  		console.log("WE HAVE " + resAmt + " ITEMS LEFT");
-		                    	console.log("Oops!, we will get more of that item soon, for now we have " + resAmt + " items in stock.");
-		                    	_this.prompt();
-
-		                    } 
-
-		                   //UPDATE_ITEM INSIDE READ_ITEM FUNCTION
-		                  	connection.query("UPDATE products SET ? WHERE ?", 
-		                  	  [{
-		                  	     stock_quantity: resAmt - prodTotal
-		                  	   }, 
-		                  	   {
-		                  	     item_id: productID
-		                  	  }], function(err, res) {
-		                  	  	// console.log("res");
-		                  	  	console.log("Updating database stock quantity...\n");
-		                  	  	if (err) throw err;
-		                  	  	
-		                  	  	// console.log(res);
-		                  	  	console.log(prodTotal + " " + itemBought + " items in your shopping cart!\n");
-		                  	  	
-		                  	  	//REPEAT PROCESS OR EXIT
-		                  	  	inquirer.prompt(morePurchase).then(function(user){
+		                if(prodTotal > resAmt){
+		                   console.log("YOU REQUESTED " + prodTotal + " ITEMS");
+		                   console.log("WE HAVE " + resAmt + " ITEMS LEFT");
+		                   console.log("Oops!, we will get more of that item soon, for now we have " + resAmt + " items in stock.");
+		                   _this.prompt();
+		                }
+		                //UPDATE_ITEM INSIDE READ_ITEM FUNCTION
+		                connection.query("UPDATE products SET ? WHERE ?", 
+		                    [{
+		                      stock_quantity: resAmt - prodTotal
+		                    }, 
+		                    {
+		                      item_id: productID
+		                    }], function(err, res) {
+		                     // console.log("res");
+		                     console.log("Updating database stock quantity...\n");
+		                     if (err) throw err;
+		                     
+		                     // console.log(res);
+		                     console.log(prodTotal + " " + itemBought + " items in your shopping cart!\n");
+		                     
+		                        //REPEAT PROCESS OR EXIT
+		                        inquirer.prompt(morePurchase).then(function(user){
 		                  	  	   if (err) throw err;
 		                  	  	   console.log(user.id);
 		                  	  	   if(user.id == "Maybe later..."){
-		                  	  	   	console.log("T'was Great to serve you, see you soon!");
-		                  	  	   	// connection.end();
-		                  	  	   	return;
-		                  	  	   	
-		                  	  	    } 
-		                  	  	    else if(user.id == "See more cool stuff!..."){
-		                  	  	    	return _this.prompt(); 
-		                  	  	    }
+		                  	  	 	 console.log("T'was Great to serve you, see you soon!");
+		                  	  	 	 // connection.end();
+		                  	  	 	 return;
+		                  	  	 	
+		                  	  	   } 
+		                  	  	     else if(user.id == "See more cool stuff!..."){
+		                  	  	     	re_readItem();
+		                  	  	     	 _this.prompt();
+		                  	  	  	    
+		                  	  	  }
 		                  	  	});
 		                  	});
 	    			   }
-					   connection.end();
-				   } 
+				    } 
 			   });
 			}//end function readItem
 		});
 	}//end prompt function
 }
 
+function re_readItem(){	
+	connection.query("SELECT * FROM products", function(err, res){
+	if (err) throw err;
+	    // console.log(res);
+       console.log("WELCOME, THANKS FOR CHOOSING BAMAZON!");
+  	   for (var i = 0; i < res.length; i++) {
+          console.log("======~~~~~~~~~~ ITEM ID:"+ res[i].item_id +" ~~~~~~~~~~=======\n");
+  	      console.log("PRODUCT NAME: " +res[i].product_name);
+  	      console.log("DEPARTMENT:   " +res[i].department_name);
+  	      console.log("PRICE:        " +res[i].price);
+          console.log("IN STOCK:     " +res[i].stock_quantity+" left!" +"\n\n\n");
+  	   }
+	})
+}
 module.exports = Customer;
